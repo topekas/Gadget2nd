e27436d567e949791febce5ba6229ea93475b294
topekas <93250733+topekas@users.noreply.github.com>
2021-11-27 18:53:50 +0700

    Update data_barang.php

--- application/views/admin/data_barang.php
+++ application/views/admin/data_barang.php
@@ -1,4 +1,4 @@
-<div class="container-fluid"
+<div class="container-fluid">
     <button type="button" class="btn btn-sm btn-primary mb-3" data-toggle="modal" data-target="#exampleModal"><i class="fas fa-plus fa-sm"></i> Tambah Barang</button>
     <table class="table table-bordered">
         <tr>
