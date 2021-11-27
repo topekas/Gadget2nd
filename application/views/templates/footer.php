0ee6bdc9537d4ad0f003b1081026044fba2643db
topekas <93250733+topekas@users.noreply.github.com>
2021-11-27 18:16:14 +0700

    Update footer.php

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,4 +1,4 @@
- <!-- Bootstrap core JavaScript-->
+ <!-- Bootstrap core JavaScript--
  <script src="<?php echo base_url() ?>assets/vendor/jquery/jquery.min.js"></script>
     <script src="<?php echo base_url() ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
