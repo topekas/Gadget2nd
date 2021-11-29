c2b5d6672c2ce9df5838ecf222e6db2517f8a95a
y0cxan <yxnhaliwela@gmail.com>
2021-11-29 15:27:28 +0700

    menambahkan file dasboard_admin.php di controllers

---
+++ application/controllers/dasboard_admin.php
@@ -0,0 +1,10 @@
+<?php
+class Dasboard_admin extends CI_Controller{
+
+    public function index(){
+        $this->load->view('templates_admin/header');
+        $this->load->view('templates_admin/sidebar');
+        $this->load->view('admin/dasboard_admin');
+        $this->load->view('templates_admin/footer');
+    }
+}
